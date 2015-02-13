ebad13f93bf6456c0cd9db90baeb97c7e10573ba
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-13 10:59:52 -0800

    add passing tests for scalene and valid triangles.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@ var triType = function(side1, side2, side3) {
   var type = null
   if (side1 === side2 && side2 === side3) {
     type = "equilateral"
-  } else if (([side1, side2, side3].indexOf(side1 || side2 || side3) > -1) && (side1 + side2 >= side3 && side2 + side3 >= side1 && + side3 >= side2)) {
+  } else if ((side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) && (side1 === side2 || side2 === side3 || side1 === side3)) {
     type = "isosceles"
-  } else {
+  } else if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) {
     type = "scalene"
+  } else {
+    type = "not a valid triangle"
   }
   return type;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,9 @@
 describe('triType', function() {
+
+  it("checks for a valid triangle", function() {
+    expect(triType(2,4,7)).to.equal("not a valid triangle");
+  });
+
   it("checks for an equilateral triangle", function() {
     expect(triType(2,2,2)).to.equal("equilateral");
   });
@@ -6,4 +11,9 @@ describe('triType', function() {
   it("checks for an isosceles triangle", function() {
     expect(triType(2,2,4)).to.equal("isosceles");
   });
+
+  it("checks for a scalene triangle", function() {
+    expect(triType(3,4,7)).to.equal("scalene");
+  });
+  
 });
