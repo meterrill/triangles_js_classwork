c23027344392f90af4e23efa21ff9079f950bd36
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-16 19:39:21 -0800

    move validation from jQuery to function and add spec coverage.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
 var triType = function(side1, side2, side3) {
-  return (side1 === side2 && side2 === side3) && 'Equilateral' ||
-  (side1 === side2 || side1 === side3 || side2 === side3) && 'Isosceles' ||
-  'Scalene';
+  if ((isNaN(side1) || isNaN(side2) || isNaN(side3)) || (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2))  {
+    return "Not A Valid Triangle!";
+  } else {
+    return (side1 === side2 && side2 === side3) && 'Equilateral' ||
+    (side1 === side2 || side1 === side3 || side2 === side3) && 'Isosceles' ||
+    'Scalene';
+  }
 };
 
 $(document).ready(function() {
   $("form#tri_input").submit(function(event) {
+    event.preventDefault();
 
     var side1 = parseFloat($('input#side_1').val());
     var side2 = parseFloat($('input#side_2').val());
     var side3 = parseFloat($('input#side_3').val());
 
-    if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
-      $(".error").text("All 3 sides must be present and in numerical form!");
-      $("#results").hide();
-      $("#errors").show();
-    } else if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
-      $(".error").text("Those dimensions do not make a valid triangle! a + b >= c");
-      $("#results").hide();
-      $("#errors").show();
-    } else {
-      var result = triType(side1, side2, side3);
-      $(".result").text(result);
-      $("#errors").hide();
-      $("#results").show();
-    }
-    event.preventDefault();
+    var result = triType(side1, side2, side3);
+    $(".result").text(result);
+    $("#results").show();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,23 @@
 describe('triType', function() {
 
+  it("will reject invalid dimensions", function() {
+    expect(triType(2,3,5)).to.equal("Not A Valid Triangle!");
+  });
+
+  it("will reject missing or non-numerical form entries", function() {
+    expect(triType(2,3,"")).to.equal("Not A Valid Triangle!");
+  });
+
   it("checks for an equilateral triangle", function() {
     expect(triType(2,2,2)).to.equal("Equilateral");
   });
 
   it("checks for an isosceles triangle", function() {
-    expect(triType(2,2,4)).to.equal("Isosceles");
+    expect(triType(2,2,3)).to.equal("Isosceles");
   });
 
   it("checks for a scalene triangle", function() {
-    expect(triType(3,4,7)).to.equal("Scalene");
+    expect(triType(3,4,5)).to.equal("Scalene");
   });
 
 });
