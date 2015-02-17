4c3bcffa5967837f6b94e2efea69c7582ffaf933
Matthew Terrill and Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-17 14:17:17 -0800

    Add ability to store triangles in lists according to type

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,34 @@
-var triType = function(side1, side2, side3) {
-  if ((isNaN(side1) || isNaN(side2) || isNaN(side3)) || 
-    (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2)) {
-    return "Not A Valid Triangle!";
-  } else {
-    return (side1 === side2 && side2 === side3) && 'Equilateral' ||
-    (side1 === side2 || side1 === side3 || side2 === side3) && 'Isosceles' ||
-    'Scalene';
-  }
-};
-
 $(document).ready(function() {
   $("form#tri_input").submit(function(event) {
     event.preventDefault();
 
-    var side1 = parseFloat($('input#side_1').val());
-    var side2 = parseFloat($('input#side_2').val());
-    var side3 = parseFloat($('input#side_3').val());
+    var inputtedSide1 = parseFloat($("input#side_1").val());
+    var inputtedSide2 = parseFloat($("input#side_2").val());
+    var inputtedSide3 = parseFloat($("input#side_3").val());
 
-    var result = triType(side1, side2, side3);
+    var newTriangle = { side1: inputtedSide1,
+                        side2: inputtedSide2,
+                        side3: inputtedSide3,
+                        typeOfTriangle: function() {
+                         if ((isNaN(this.side1) || isNaN(this.side2) || isNaN(this.side3)) ||
+                           (this.side1 + this.side2 <= this.side3 ||
+                           this.side2 + this.side3 <= this.side1 ||
+                           this.side1 + this.side3 <= this.side2)) {
+                           return "Not A Valid Triangle!";
+                         } else {
+                           return (this.side1 === this.side2 && this.side2 === this.side3) && 'equilateral' ||
+                           (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3) && 'isosceles' ||
+                           'scalene';
+                         }
+                       }
+                      };
 
-    if (result === "Not A Valid Triangle!") {
-      $(".result").hide();
-      $(".error").text(result);
-      $(".error").show();
+    var newTriangleType = { type: newTriangle.typeOfTriangle()
+                          };
+    if (newTriangleType.type === "Not A Valid Triangle!") {
+      alert (newTriangleType.type);
     } else {
-      $(".error").hide();
-      $(".result").text(result);
-      $(".result").show();
+      $("ul#" + newTriangleType.type).append("<li><span class='type'>" + newTriangle.side1 + ", " + newTriangle.side2 + ", " + newTriangle.side3 + "</span></li>");
     }
-    $("#results").show();
   });
 });
